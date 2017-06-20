2cb9b86ef5acfa7aef1904224eb87c41d1f13db3
Amanda Koster <amandakoster@gmail.com>
2017-06-19 18:41:40 -0700

    quit and nick are done

--- lab-amanda/index.js
+++ lab-amanda/index.js
@@ -2,42 +2,57 @@
 
 const net = require('net');
 const server = net.createServer();
+const Client = require('./constructor.js');
+let clientPool = [];
 
-// let clientPool = [];
-//
-// server.on('connection', (user) => {
-//   user.write('Hello user, welcome to this chatbox!\n');
-//   user.nickname = `user_${Math.random()}`;
-//   console.log(`${user.nickname} connected to chat!`);
-//
-//   clientPool = [...clientPool, user];
-//
-//   let handleDisconnect = () => {
-//     console.log(`${user.nickname} has left the chat.`);
-//     clientPool = clientPool.filter(item => item !==user);
-//   };
-//
-//   user.on(`error`, handleDisconnect);
-//   user.on('close', handleDisconnect);
-//
-//   user.on('data', (buffer) => {
-//     let data = buffer.toString();
-//     if(data.startsWith('/nickname ')){
-//       user.nickname = data.split('/nickname ')[1] || user.nickname;
-//       user.nickname = user.nickname.trim();
-//       user.write(`your chat name is now ${user.nickname}`);
-//       return;
-//     }
-//
-//     if(data.startsWith('/dm')) {
-//       let content = data.split('/dm')[1] || '';
-//     }
-//
-//     clientPool.forEach((item) => {
-//       item.write(`${user.nickname}: ${data}`);
-//     });
-//   });
-// });
+server.on('connection', (socket) => {
+  let client = new Client(socket);
+  socket.write('Hello socket, welcome to this chatbox!\n');
+  console.log(`${client.nickname} connected to chat!`);
+
+  clientPool = [...clientPool, socket];
+
+  let handleDisconnect = () => {
+    console.log(`${client.nickname} has left the chat.`);
+    clientPool = clientPool.filter(item => item !==socket);
+  };
+
+  socket.on('error', handleDisconnect);
+  socket.on('close', handleDisconnect);
+
+  socket.on('data', (buffer) => {
+    let data = buffer.toString();
+
+    if(data.startsWith('/dm')) {
+      let username = data.split('')[1];
+      let matchedUsers = clientPool.filter;
+      let user =
+      content = content.trim();
+      clientPool.filter(client);
+
+    }
+
+    // if(data.startsWith('/troll')) {
+    //   let content = data.split('/dm')[1] || '';
+    //   console.log('line 29', client);
+    //   clientPool.filter(client);
+    // }
+
+    if(data.startsWith('/nick')) {
+      let content = data.split('/nick')[1] || '';
+      client.nickname = content;
+    }
+
+    clientPool.forEach((item) => {
+      item.write(`${client.nickname}: ${data}`);
+
+    });
+
+    if(data.startsWith('/quit')) {
+      socket.end();
+    }
+  });
+});
 
 server.listen(3000, () => {
   console.log('server up on port 3000');
